5bf78a531643e9f029f3a6e911b79101e8c4443f
Michael Propp <michaelpropp8664@gmail.com>
2020-05-12 16:00:07 -0400

    Initial Navigation

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+        return CategoryMealsScreen();
+      }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.headline6),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [color.withOpacity(0.7), color])),
+    return InkWell(
+      onTap: () {selectCategory(context);},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title, style: Theme.of(context).textTheme.headline6),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [color.withOpacity(0.7), color])),
+      ),
     );
   }
 }
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("The Recipes")),
+      body: Center(child: Text("Recipes for the category")),
+    );
+  }
+}
