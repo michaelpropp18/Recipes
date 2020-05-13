98a43528f833f20188d8c85290341eed01dcb635
Michael Propp <michaelpropp8664@gmail.com>
2020-05-13 19:18:35 -0400

    Tabs pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
