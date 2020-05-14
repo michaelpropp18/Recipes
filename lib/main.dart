ac8745748940bb415e3f1d1aca48f84d14ac28a3
Michael Propp <michaelpropp8664@gmail.com>
2020-05-13 21:26:22 -0400

    Navigation Side Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:recipes/screens/meal_detail_screen.dart';
 
+import './screens/filters_screen.dart';
+import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/tabs_screen.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       /*
       onGenerateRoute: (settings) {
